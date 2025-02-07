076fbbf96434924cf26649b0d4943924ed3e72eb
Godwin Mat-ao <adwinkoaclc@gmail.com>
2025-02-08 01:55:20 +0800

    Added Loading Indicatiors when fetching data

--- src/app/components/data-table/data-table.component.ts
+++ src/app/components/data-table/data-table.component.ts
@@ -4,18 +4,28 @@ import { AddUserComponent } from '../add-user/add-user.component';
 import { EmptyListComponent } from '../empty-list/empty-list.component';
 import { CommonModule } from '@angular/common';
 import { UserService } from '../../user.service';
+import { ServerOfflineComponent } from '../server-offline/server-offline.component';
+import { LoadingComponent } from '../loading/loading.component';
 
 @Component({
   selector: 'app-data-table',
   standalone: true,
   templateUrl: './data-table.component.html',
   styleUrls: ['./data-table.component.scss'],
-  imports: [AddUserComponent, EmptyListComponent, CommonModule],
+  imports: [
+    AddUserComponent,
+    EmptyListComponent,
+    CommonModule,
+    ServerOfflineComponent,
+    LoadingComponent,
+  ],
 })
 export class DataTableComponent implements OnInit {
   users: any[] = [];
   error: string | null = null;
   selectedUser: any = null;
+  isLoading: boolean = false;
+  loadingDelay: number = 500;
 
   constructor(private userService: UserService, private router: Router) {}
 
@@ -24,11 +34,22 @@ export class DataTableComponent implements OnInit {
   }
 
   fetchUsers(): void {
+    this.isLoading = true;
     this.userService.getUsers().subscribe(
-      (data) => (this.users = data),
+      (data) => {
+        this.users = data;
+        setTimeout(() => {
+          this.isLoading = false;
+        }, this.loadingDelay);
+      },
+
       (error) => {
+        this.isLoading = true;
         console.error('Error fetching users:', error);
         this.error = 'Error fetching users. Please try again later.';
+        setTimeout(() => {
+          this.isLoading = false;
+        }, this.loadingDelay);
       }
     );
   }
---
+++ src/app/components/server-offline/server-offline.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-server-offline',
+  standalone: true,
+  imports: [],
+  templateUrl: './server-offline.component.html',
+  styleUrl: './server-offline.component.scss',
+})
+export class ServerOfflineComponent {}
